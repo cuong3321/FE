7c18b0d4306780dd2fab542dadf59f065d53fd0d
cuong tran <cuong.it.533@gmail.com>
2023-06-13 16:01:59 +0700

    sdsd

---
+++ build/precache-manifest.eccc2646bd7a1fb2bea916a30aea87ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774d7951d73a69844ffacf72dece021b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2031d84db0a4960c990d",
+    "url": "/static/css/main.2e2ddb7f.chunk.css"
+  },
+  {
+    "revision": "207d7faff056466f3c63",
+    "url": "/static/js/2.dab92086.chunk.js"
+  },
+  {
+    "revision": "06a879a6a1f813869392cf625f2fa3f3",
+    "url": "/static/js/2.dab92086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2031d84db0a4960c990d",
+    "url": "/static/js/main.d5126e52.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
